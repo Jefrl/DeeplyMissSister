ebe0c4bd2750ea36e9a53fe691bd974b3db5ecc8
Jefrl <88300048@qq.com>
2017-02-27 02:30:06 +0800

    git init

---
+++ BaiSiBuDeJie/Classes/Other/System/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiSiBuDeJie
+//
+//  Created by Jefrl on 17/2/26.
+//  Copyright © 2017年 com.Jefrl.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiSiBuDeJie/Classes/Other/System/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaiSiBuDeJie
+//
+//  Created by Jefrl on 17/2/26.
+//  Copyright © 2017年 com.Jefrl.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
